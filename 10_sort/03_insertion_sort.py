236fcfbc2ea5926a5ec71a3444273a879d414658
pryadavgrid <pryadav@griddynamics.com>
2026-04-07 11:32:28 +0530

    add second method in insertion sort

--- 10_sort/03_insertion_sort.py
+++ 10_sort/03_insertion_sort.py
@@ -22,3 +22,21 @@ print(sorted_list_using_insertion_sort)
 
 
 
+#  Method 2
+
+def insertion_sort_method_two(my_list):
+    for i in range(1, len(my_list)):
+        key = my_list[i]
+        j = i - 1
+
+        while j >= 0 and my_list[j] > key:
+            my_list[j + 1] = my_list[j]
+            j -= 1
+
+        my_list[j + 1] = key
+
+    return my_list
+
+my_list = [64,25,32,20,40,15]
+sorted_list_using_insertion_sort_method_two = insertion_sort_method_two(my_list)
+print(sorted_list_using_insertion_sort_method_two)
